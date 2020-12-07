919650293ae8ad6ebcc2c286c49dabfab3836ad9
sadafproducts <sadaf.products@gmail.com>
2020-12-07 12:18:53 +0100

    Update rename and move file.py

--- rename and move file.py
+++ rename and move file.py
@@ -1,11 +1,17 @@
-# Program 1
+# Working with system files
+#   1 - create a file
+#   2 - create a folder
+#   3 - rename a file
+#   4 - move a file
+# Pay attention to r before the paths: escape the backslashes or use raw strings: "C:\\Users..." or r"C:\Users..."
 
-# This code renames a file and moves it to a new folde.
+open(r"D:\Python01_test.txt", "a") # Create a file
+import os   # Add os lib to use mkdir and rename commands
+os.mkdir(r"D:\_PythonTempTest0") # Create a direcory/folder
+os.rename(r"D:\Python01_test.txt", r"D:\Python01.txt") # Rename file 
+os.rename(r"D:\Python01.txt", r"D:\_PythonTempTest0\01_file.txt") # Move the file to the new directroy you created
 
-import os
-
-os.rename(r"C:\Users\Muhammad\Desktop\NewJobsCopy\1.pdf", r"C:\Users\Muhammad\Desktop\NewJobsCopy\1-ad.pdf") # Rename file - If error: You should probably escape the backslashes or use raw strings: "C:\\Users..." or r"C:\Users..."
-
-os.mkdir(r"C:\Users\Muhammad\Desktop\NewJobsCopy\1") # Create a direcory
-
-os.rename(r"C:\Users\Muhammad\Desktop\NewJobsCopy\1-ad.pdf", r"C:\Users\Muhammad\Desktop\NewJobsCopy\1\1-ad.pdf") # Move the file to the new directroy you created above
+# Create another file and then delete it
+open(r"D:\_PythonTempTest0\02_file.txt", "a")
+os.remove(r"D:\_PythonTempTest0\02_file.txt") 
+# End of this example.
